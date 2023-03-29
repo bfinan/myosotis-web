134171719789de8c02afb01547134f3e0138b77d
Brendan Finan <bfinan.py@gmail.com>
2023-03-29 13:54:23 -0500

    initial stab at page routing

---
+++ pages/tools/intake.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Intake</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+
+
+    </>
+  );
+}
---
+++ pages/tools/pacer.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Pacer</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+
+      <h3>Title</h3><br></br>
+      <input></input><br></br>
+      starting location
+      <input></input><br></br>
+      end location 
+    <input></input>
+
+    
+    </>
+  );
+}
