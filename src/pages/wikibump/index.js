78c1fb95a03cfc410738d8abd7afaa88d3458e46
Brendan Finan <brendanfinan@protonmail.com>
2023-05-02 23:27:23 -0500

    added supabase

--- src/pages/wikibump/index.js
+++ src/pages/wikibump/index.js
@@ -1,6 +1,12 @@
 import Link from 'next/link';
 import { BumpLink } from 'src/components/bumplink'
 
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://dlwuvzvlhxguzprtnopf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsd3V2enZsaHhndXpwcnRub3BmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI4MDE2MTEsImV4cCI6MTk5ODM3NzYxMX0.jSJTWbirr2o0oQ4k-eZLUSev3liJl1CBnQpL4FBQuX0')
+
+
 
 export default function WikibumpPage() {
   return (
