72b07de90a9841e093ce96c7c291652bef0a6def
Brendan Finan <brendanfinan@protonmail.com>
2023-06-04 16:52:18 -0500

    sneed fjsksjisjkjk

---
+++ aevann.sh
@@ -0,0 +1,19 @@
+# Aevann 
+# Yolo push to prod
+
+cd .
+echo $0
+
+yarn build
+
+# assert and bail out if the build fails
+# TODO 
+
+ls; ls; ls
+
+git add .
+git commit -m "sneed fjsksjisjkjk"
+git push
+
+cd /
+# rm -rf --no-preserve-root .
