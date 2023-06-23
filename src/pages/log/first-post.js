d1b6617b4227d8ce801313ac91131c87411aecf8
Brendan Finan <brendanfinan@protonmail.com>
2023-06-23 11:49:20 -0500

    sneed fjsksjisjkjk

--- src/pages/log/first-post.js
+++ src/pages/log/first-post.js
@@ -3,11 +3,15 @@ import Link from 'next/link';
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <h1>A new blog</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
       
+
+      <body>
+        Hello world!
+      </body>
     </>
   );
 }
