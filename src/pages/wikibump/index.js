2749c4471c8b0b9da7a2c973bca912ef96436eef
Brendan Finan <brendanfinan@protonmail.com>
2023-07-08 14:18:03 -0500

    Hyphenate

--- src/pages/wikibump/index.js
+++ src/pages/wikibump/index.js
@@ -27,7 +27,7 @@ export default function WikibumpPage() {
         <BumpLink title="Prediction Market" wikititle='https://www.wikipedia.org/wiki/Prediction_Market'></BumpLink>
         <BumpLink title="Gastrocnemius Muscle" wikititle='https://www.wikidata.org/wiki/Q943203'></BumpLink>
         <BumpLink title="Glycogen" wikititle='https://en.wikipedia.org/wiki/Glycogen'></BumpLink>
-        <BumpLink title="Lactic Acid" wikititle='https://en.wikipedia.org/wiki/Lactic_acid'></BumpLink>
+        <BumpLink title="Lactic-Acid" wikititle='https://en.wikipedia.org/wiki/Lactic_acid'></BumpLink>
         <BumpLink title="Contango" wikititle='https://en.wikipedia.org/wiki/Contango'></BumpLink>
         <BumpLink title="Random-access-memory" wikititle='https://en.wikipedia.org/wiki/Random-access_memory'></BumpLink>
         
