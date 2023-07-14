5430d389f01c01db7a5eaebcfa92f8128f7b8173
Brendan Finan <brendanfinan@protonmail.com>
2023-07-14 13:12:34 -0500

    updated about

--- public/logo.md
+++ public/logo.md
@@ -1,7 +1,14 @@
 # logo
 
-## Spec 
+## Spec
+
 svg logo of a counterclockwise spiral, starting as indigo and shifting to yellow as the spiral circles inward
 
-(logo should be a clockwise spiral when displayed in the southern hemisphere due to the coriolis effect) 
+## notes
+
+(logo should be a clockwise spiral when displayed in the southern hemisphere due to the coriolis effect)
 colors should be closer to the colors of the myosotis flower, a punchier version of their blue and yellow
+
+## version 1
+
+made with DALL-E. has kind of a cool optical illusion effect
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -21,7 +21,22 @@ export default function Home() {
           <Link href="/">Back</Link>
 
         <div>
-          <h2>“I think we ought to read only the kind of books that wound or stab us. If the book we&apos;re reading doesn&apos;t wake us up with a blow to the head, what are we reading for? So that it will make us happy, as you write? Good Lord, we would be happy precisely if we had no books, and the kind of books that make us happy are the kind we could write ourselves if we had to. But we need books that affect us like a disaster, that grieve us deeply, like the death of someone we loved more than ourselves, like being banished into forests far from everyone, like a suicide. A book must be the axe for the frozen sea within us.”</h2>
+          <h1>As of 2023-07-14, this website is a experiment testing how distributed teams can be used for research. Personal Knowledge Management (PKM) tools 
+            are of particular interest to this project, as well as the concept of `programmable attention`.
+          </h1>
+        </div>
+
+
+        <div>
+          <h3>Also, there`s this quote:</h3>
+          <h2>“I think we ought to read only the kind of books that wound or stab us. 
+            If the book we&apos;re reading doesn&apos;t wake us up with a blow to the head,
+            what are we reading for? So that it will make us happy, as you write? 
+            Good Lord, we would be happy precisely if we had no books, and the kind of books that make us happy 
+            are the kind we could write ourselves if we had to. But we need books that affect us like a disaster,
+             that grieve us deeply, like the death of someone we loved more than ourselves,
+              like being banished into forests far from everyone, like a suicide. 
+              A book must be the axe for the frozen sea within us.”</h2>
           <h3>--Franz Kafka</h3>
         </div>
 
