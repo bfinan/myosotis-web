8fca9d291a5617e20c26a73e42ac5084d251e01e
Brendan Finan <brendanfinan@protonmail.com>
2023-08-10 17:32:49 -0500

    className?

--- src/pages/wikibump/index.js
+++ src/pages/wikibump/index.js
@@ -20,8 +20,7 @@ export default function WikibumpPage() {
       <h3>User: Brendan Finan</h3>
       <h2>
         <Link href="/"  >Back to home</Link> <br></br>
-        <BumpLink title="Niacin" wikititle='https://www.wikipedia.org/wiki/Niacin'></BumpLink>
-
+        <BumpLink class="square" title="Niacin" wikititle='https://www.wikipedia.org/wiki/Niacin'></BumpLink>
         <BumpLink title="Running" wikititle='https://www.wikipedia.org/wiki/Running'></BumpLink>
         <BumpLink title="Boston Marathon" wikititle='https://www.wikipedia.org/wiki/Boston_Marathon'></BumpLink>
         <BumpLink title="Datepicker" wikititle='https://www.wikidata.org/wiki/Q25345907'></BumpLink>
